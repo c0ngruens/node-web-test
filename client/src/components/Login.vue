cec1c716fe96299297c1daec1fda64a766e1a561
c0ngruens <vlada.ramone@gmail.com>
2018-06-10 16:33:49 +0200

    Login page

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,25 +1,43 @@
 <template>
-  <div class="page-container">
-    <form novalidate class="md-layout md-alignment-center-center" @submit.prevent="validateUser">
-      <md-card class="md-layout-item md-size-33 md-medium-size-60 md-small-size-75 md-xsmall-size-100">
-        <md-card-header>
-          <div class="md-title">Login</div>
+  <div style="min-height: 100vh; overflow-x: hidden;" class="md-layout">
+    <form novalidate class="md-layout md-gutter md-alignment-center-center" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-33 md-large-size-33 md-medium-size-45 md-small-size-75 md-xsmall-size-90">
+        <md-card-header class="md-layout md-alignment-center">
+          <div class="md-title">
+            <span>Login</span>
+          </div>
         </md-card-header>
         <md-card-content>
+          <md-field>
+            <label>Email</label>
+            <md-input v-model="email" type="email"></md-input>
+          </md-field>
+          <md-field>
+            <label>Password</label>
+            <md-input v-model="password" type="password"></md-input>
+          </md-field>
         </md-card-content>
+        <md-card-actions class="md-layout md-alignment-center">
+          <md-button class="md-raised md-primary">Enter</md-button>
+        </md-card-actions>
+        <div class="space"></div>
       </md-card>
     </form>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    username: null
+    email: null,
+    password: null
   })
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.space {
+  padding-bottom: 8px;
+}
 </style>
